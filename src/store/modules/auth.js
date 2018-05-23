85bcb4351308a53887855b5380f45c075c5be7fa
Radislaw <radislaw22@gmail.com>
2018-05-23 19:27:16 +1000

    add vuex modules

---
+++ src/store/modules/auth.js
@@ -0,0 +1,28 @@
+import api from '../../api/imgur'
+
+const state = {
+  token: null
+};
+
+const getters = {
+  isLoggedIn: state => !!state.token
+};
+
+const mutation = {
+  setToken(state, token) {
+    state.token = token;
+  }
+};
+
+const actions = {
+  login() {
+    api.login();
+  },
+  logout({ commit }) {
+    commit('setToken', null)
+  }
+}
+
+export default {
+  state, getters, actions, mutation,
+}
